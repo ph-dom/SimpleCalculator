b32b0fa2bd46eed2cb61a76a99cb022f5922b00e
Pablo Henriquez <p.heriquezd@outlook.com>
2022-04-19 14:39:50 -0400

    Initial Commit

---
+++ SimpleCalculator/SimpleCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleCalculatorApp.swift
+//  SimpleCalculator
+//
+//  Created by Pablo Henriquez on 19-04-22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
